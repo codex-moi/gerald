7869165d3e8fadcb93859517940d03d2cd189b48
Gerald <gerald.olarichie@gmail.com>
2025-05-02 07:06:01 +0100

    Added project tracking and updated Projects component

--- src/assets/projects.js
+++ src/assets/projects.js
@@ -1,26 +1,34 @@
+import PortPic from '../assets/PortPic.png'
+import Klothify from '../assets/Klothify.png'
+import Filmland from '../assets/Filmland.png'
+import KryptoKing from '../assets/KryptoKing.png'
 const projects = [
     {
         id: 1,
+        image: Klothify,
         name: 'Klothify',
-        desc: 'A clothing ecommerce website that shows designers.',
+        desc: 'An e-commerce platform showcasing unique clothing designs with a seamless shopping experience.',
         link: 'https://klothifyy.vercel.app/'
     },
     {
         id: 2,
+        image: PortPic,
         name: 'My Portfolio',
-        desc: 'My portfolio website that shows my projects and skills.',
+        desc: 'A personal portfolio website to showcase my projects, skills, and achievements.',
         link: 'https://geraldcodez.vercel.app/'
     },
     {
         id: 3,
+        image: Filmland,
         name: 'Filmland',
-        desc: 'Movie website using API.',
+        desc: 'A movie discovery platform where users can explore popular films and their details.',
         link: 'https://geraldfilmland.vercel.app/'
     },
     {
         id: 4,
+        image: KryptoKing,
         name: 'KryptoKing',
-        desc: ' website using API.',
+        desc: 'A cryptocurrency tracker providing real-time updates on market trends and prices.',
         link: 'https://krypto-king.vercel.app/'
     },
 ]
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,15 +3,18 @@ import projects from '../assets/projects'
 const Projects = () => {
   return (
     <>
-    <div className="w-full h-[100vh] bg-black px-[3%]">
+    <div className="w-full h-auto bg-black px-[3%]">
         <h2 className="text-4xl px-[5%] text-white">My Projects</h2>
-        <div className="w-full flex flex-wrap">
+        <div className="w-full flex flex-wrap justify-center">
             {projects.map((project, id) =>
             (
-                <div className="w-[400px] h-[250px] max-md:w-[80%] max-md:mx-[10%] my-4 mr-4 rounded bg-[#5e5f5f] text-white bg-opacity-70 border-cyan-500 border p-2 max-sm:p-1 hover:scale-105 hover:transition-all hover:duration-500" key={id}>
-                    <h2>{project.name} </h2>
+                <div className="w-[350px] h-auto max-md:w-[80%] max-md:mx-auto my-4 mr-4 rounded bg-[#5e5f5f] text-white bg-opacity-70 border-cyan-500 border p-2 max-sm:p-1 hover:scale-105 hover:transition-all hover:duration-500" key={id}>
+                   <div className="w-[100%]">
+                    <img src={project.image} className='w-full' alt="" />
+                   </div>
+                    <h2 className='text-2xl text-[#4dc6eb]'>{project.name} </h2>
                     <p>{project.desc} </p>
-                    <a href={project.link}>Link</a>
+                    <a href={project.link} className='text-[#4dc6eb]'>{project.link}</a>
                 </div>
             )
             )}
